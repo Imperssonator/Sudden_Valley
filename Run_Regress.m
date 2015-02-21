87a1d0fb3cc16f670bdc37e594b0a741fe13de96
Nils <imperssonator@gmail.com>
2015-02-20 20:07:09 -0500

    updated documentation

--- Run_Regress.m
+++ Run_Regress.m
@@ -2,13 +2,16 @@ function [REG, ERR] = Run_Regress(Data,R,WhichVars,N,polyDeg,VarNames)
 % Take filled-in data matrix as input, and vector of mobilities R, as well
 % as 'WhichVars', a row vector of which variables of Data to use, and N,
 % how many of those to use at a time at max
+% polyDeg is the maximum degree of polynomial to consider
+% VarNames is a cell array of strings that correspond to the variables in
+% Data, i.e. it is the "Row_Contents" output of Struct2Data
 
 %% Create permutation vectors
 % In order to run regressions on all possible combinations of the chosen
 % variables
 Combos = {};
 
-for i = 1:N
+for i = 1:N % combinations of variables from Data up to groups of N
     Combosi = nchoosek(WhichVars,i);
     for j = 1:length(Combosi)
         Combos = [Combos; Combosi(j,:)];
